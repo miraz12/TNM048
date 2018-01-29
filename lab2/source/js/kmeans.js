c8143c752ddae55354421aeab4786d0aa46f583c
Anton Christoffersson <anton_christoffersson@hotmail.com>
2018-01-29 11:52:25 +0100

    init commit lab2

---
+++ lab2/source/js/kmeans.js
@@ -0,0 +1,15 @@
+/**
+* k means algorithm
+* @param data
+* @param k
+* @return {Object}
+*/
+
+function kmeans(data, k) {
+
+    //Implement the algorithm here..
+    //Remember to reference any code that you have not implemented yourself! 
+
+};
+
+
